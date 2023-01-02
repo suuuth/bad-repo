cc593e176334cc5201b4cf73e5dc3e56686ec441
suuuth <kevin.sutherl@gmail.com>
2023-01-02 17:55:24 -0500

    fixing stuff

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
 
-import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get, Render } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
