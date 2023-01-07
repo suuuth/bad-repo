6676d37dea8222dfc1b6e28a8d88dfb75d81d79a
suuuth <kevin.sutherl@gmail.com>
2023-01-06 21:31:38 -0500

    ormconfig

---
+++ ormconfig.ts
@@ -0,0 +1,13 @@
+
+const config: PostgreSQL = {
+  type: 'postgres',
+  host: 'postgres',
+  port: 5432,
+  username: 'postgres',
+  password: 'Password123!',
+  database: 'postgres',
+  entities: [],
+  synchronize: true,
+}
+
+export default config;
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,24 +6,16 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
+import config from '../ormconfig';
 
 @Module({
   imports: [
-    UsersModule, 
-    AuthModule, 
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'postgres',
-      port: 5432,
-      username: 'postgres',
-      password: 'Password123!',
-      database: 'postgres',
-      entities: [],
-      synchronize: true,
-    }),
+    UsersModule,
+    AuthModule,
+    TypeOrmModule.forRoot(config),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client/dist'),
-    })
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
